10f16d23169f36baac42dd1cee289b8bd75dcd0c
Cean Tolentino <cean.tolentino@gmail.com>
2019-09-11 15:30:04 -0700

    pushing restful_task_api

---
+++ mongo/restful_task_api/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost/taskdb', { useNewUrlParser: true });
+
+const TaskSchema = new mongoose.Schema({
+    title: String,
+    description: { type: String, default: "" },
+    completed: { type: Boolean, default: false },
+}, { timestamps: true })
+
+const Task = mongoose.model("Task", TaskSchema)
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.get('/tasks', (req, res) => {
+    Task.find()
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+});
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+})
+
+app.post('/tasks', (req, res) => {
+    console.log(req.body)
+    Task.create(req.body)
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+})
+
+app.put('/tasks/:id', (req, res) => {
+    Task.findByIdAndUpdate(req.params.id, req.body)
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+})
+
+app.delete('/tasks/:id', (req, res)=>{
+    Task.findByIdAndDelete(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => res.json(err))
+})
+
+app.listen(8000, () => console.log("listening on port 8000"));
